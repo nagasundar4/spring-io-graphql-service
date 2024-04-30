549b8e418c18c458afc55cd0954b1c147c71aa29
Nagarajan Sundaram <nagasundar@gmail.com>
2024-04-30 19:16:55 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+echo "# GraphQL-service" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/nagasundar4/GraphQL-service.git
+git push -u origin main
+
+git remote add origin https://github.com/nagasundar4/GraphQL-service.git
+git branch -M main
+git push -u origin main
---
+++ src/main/java/com/nagasystems/graphqlservice/GraphQlServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nagasystems.graphqlservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphQlServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphQlServiceApplication.class, args);
+    }
+
+}
